9efbd947a3f9fecf558c35e26c30c93b31862e83
john <borges-2014@live.com>
2021-01-20 18:50:33 -0300

    feat: Change query format and update its parser

--- mon_health/food_parser.py
+++ mon_health/food_parser.py
@@ -1,78 +1,170 @@
 import re
 from datetime import datetime
-from functools import reduce
 
 from .db import Food
-from .utils import parse_date, parse_time
+from .utils import InvalidTime, convert_to_date, convert_to_time
+
+
+class KeywordNotFound(Exception):
+    pass
+
+
+class InvalidExpression(Exception):
+    pass
+
+
+class InvalidValue(Exception):
+    pass
+
+
+class InvalidName(Exception):
+    pass
+
+
+class InvalidSortField(Exception):
+    pass
+
+
+class InvalidLimit(Exception):
+    pass
 
 
 class FoodParser:
+    exprs = [
+        {
+            "name": "name",
+            "keyword_pattern": r"name|n",
+            "value_pattern": r"[`'\"].*?[`'\"]",
+        },
+        {
+            "name": "date",
+            "keyword_pattern": r"date|d",
+            "value_pattern": r"\d{1,2}(/\d{1,2}(/\d{1,4})?)?",
+        },
+        {
+            "name": "time",
+            "keyword_pattern": r"time|t",
+            "value_pattern": r"\d{1,2}:\d{2}|\d{1,2}h+",
+        },
+        {
+            "name": "sort",
+            "keyword_pattern": r"sort|s",
+            "value_pattern": r"-?\w+(,-?\w+)*",
+        },
+        {
+            "name": "limit",
+            "keyword_pattern": r"limit|l",
+            "value_pattern": r"\d+",
+        },
+    ]
+    keyword_patterns = "|".join([e["keyword_pattern"] for e in exprs])
+
     def __init__(self, _input):
         self.input = _input
         self.where_clause = None
-        self.where_clause_filters = []
-        self.sorting_clause = None
+        self.sort_clause = None
         self.limit_clause = None
 
-    def match(self, pattern):
-        return re.match(pattern, self.input, re.IGNORECASE)
+    def parse_expr(self, *, name, keyword_pattern, value_pattern):
+        keyword, keyword_start, keyword_end = self.search_keyword(
+            keyword_pattern, self.input
+        )
+        value, _, value_end = self.match_value(
+            value_pattern, self.input[keyword_end:]
+        )
+        expr_start, expr_end = keyword_start, keyword_end + value_end
+        # whitespace is added in between since it's always matched
+        self.input = (self.input[:expr_start] + " " + self.input[expr_end:]).strip()
+        self.get_parser(name)(value)
 
     def parse(self):
-        self.parse_where_clause()
-        self.parse_sorting_clause()
-        self.parse_limit_clause()
+        for expr in self.exprs:
+            try:
+                self.parse_expr(**expr)
+            except KeywordNotFound:
+                pass
 
-    def parse_where_clause(self):
-        self.parse_name()
-        self.parse_date()
-        self.parse_time()
-        if self.where_clause_filters:
-            self.where_clause = reduce(lambda a, b: a & b, self.where_clause_filters)
-
-    def parse_name(self):
-        match = self.match(r"(.*?);\s*")
-        if match:
-            self.where_clause_filters.append(Food.name == match.groups()[0].strip())
-            self.input = self.input[match.end() :]
-
-    def parse_date(self):
-        match = self.match(r"today *")
-        if match:
-            self.where_clause_filters.append(Food.date == datetime.now().date())
-            self.input = self.input[match.end() :]
-        else:
-            match = self.match(r"(\d+$|\d+\s|\d+(/\d+)+)\s*")
-            if match:
-                self.where_clause_filters.append(
-                    Food.date == parse_date(match.groups()[0])
-                )
-                self.input = self.input[match.end() :]
-
-    def parse_time(self):
-        match = self.match(r"(\d+:\d+) *")
-        if match:
-            self.where_clause_filters.append(
-                Food.time == parse_time(match.groups()[0])
+        if self.input:
+            raise InvalidExpression(
+                f"Expression '{self.input}' could not be parsed."
             )
-            self.input = self.input[match.end() :]
+
+    def ends_with_keyword(self, string):
+        return re.search(f"({self.keyword_patterns})$", string, re.I)
+
+    def search_keyword(self, pattern, string):
+        try:
+            pattern = r"(\s+|^)(" + pattern + r")(\s+|$)"
+            match = re.search(pattern, string, re.I)
+            assert match and not self.ends_with_keyword(string[: match.start()])
+            return (match.group(2), match.start(), match.end())
+        except AssertionError:
+            raise KeywordNotFound(f"String doesn't match pattern '{pattern}'.")
+
+    def match_value(self, pattern, string):
+        try:
+            pattern = r"(\s+|^)(" + pattern + r")(\s+|$)"
+            match = re.match(pattern, string, re.I)
+            assert match
+            return (match.group(2), match.start(), match.end())
+        except AssertionError:
+            invalid_value = re.match(r"\S*", string).group()
+            raise InvalidValue(f"Value '{invalid_value}' is invalid.")
+
+    def get_parser(self, name):
+        return getattr(self, f"parse_{name}")
+
+    def add_to_where_clause(self, value):
+        if self.where_clause is None:
+            self.where_clause = value
         else:
-            match = self.match(r"(\S+)h *")
-            if match:
-                hour = match.groups()[0]
-                low = parse_time(hour + ":00")
-                high = parse_time(hour + ":59")
-                self.where_clause_filters.append(Food.time.between(low, high))
-                self.input = self.input[match.end() :]
+            self.where_clause &= value
 
-    def parse_sorting_clause(self):
-        match = self.match(r"(?:order +by|sort) +(-?)(\w+)\b *")
-        if match:
-            field = getattr(Food, match.groups()[1])
-            self.sorting_clause = field.desc() if match.groups()[0] else field
-            self.input = self.input[match.end() :]
+    def parse_name(self, string):
+        if not re.match(r"([\"'`]).*\1", string):
+            raise InvalidName("Name should be quoted.")
+        if not string[1:-1]:
+            raise InvalidName("Name can't be empty.")
 
-    def parse_limit_clause(self):
-        match = self.match(r"limit +(\d+)\b *")
-        if match:
-            self.limit_clause = match.groups()[0]
-            self.input = self.input[match.end() :]
+        self.add_to_where_clause(Food.name == string[1:-1])
+
+    def parse_date(self, value):
+        if re.match(value, "today", re.I):
+            self.add_to_where_clause(Food.date == datetime.now().date())
+        else:
+            self.add_to_where_clause(Food.date == convert_to_date(value))
+
+    def parse_time(self, value):
+        if value.endswith("h"):
+            hour = value[:-1]
+            low = convert_to_time(hour + ":00")
+            high = convert_to_time(hour + ":59")
+            self.add_to_where_clause(Food.time.between(low, high))
+        elif ":" in value:
+            self.add_to_where_clause(Food.time == convert_to_time(value))
+        else:
+            raise InvalidTime
+
+    def add_to_sort_clause(self, value):
+        if self.sort_clause is None:
+            self.sort_clause = []
+        self.sort_clause.append(value)
+
+    def parse_sort(self, value):
+        try:
+            for param in re.split(r"\s*,\s*", value):
+                if param.startswith("-"):
+                    field = getattr(Food, param[1:]).desc()
+                else:
+                    field = getattr(Food, param).asc()
+                self.add_to_sort_clause(field)
+        except AttributeError:
+            raise InvalidSortField
+
+    def parse_limit(self, value):
+        try:
+            limit = int(value)
+            assert limit >= 0
+            self.limit_clause = limit
+        except (ValueError, TypeError, AssertionError):
+            raise InvalidLimit("Limit should be a positive integer.")
--- mon_health/test_food_parser.py
+++ mon_health/test_food_parser.py
@@ -1,87 +1,364 @@
 from datetime import datetime, time
 
+import peewee
 import pytest
 
-from .food_parser import Food, FoodParser
+from .food_parser import (
+    Food,
+    FoodParser,
+    InvalidExpression,
+    InvalidLimit,
+    InvalidName,
+    InvalidSortField,
+    InvalidTime,
+    InvalidValue,
+    KeywordNotFound,
+)
 
 
 def now():
     return datetime.now()
 
 
+def isExpr(obj):
+    return isinstance(obj, peewee.Expression)
+
+
+def isField(obj):
+    return issubclass(obj.__class__, peewee.Field)
+
+
+def compare_rhs_of_between_op(rhs1, rhs2):
+    assert rhs1.nodes[0] == rhs2.nodes[0]
+    assert rhs1.nodes[1].sql == rhs2.nodes[1].sql
+    assert rhs1.nodes[2] == rhs2.nodes[2]
+    return True
+
+
+def compare_nested_exprs(expr1, expr2):
+    if isField(expr1) or isField(expr2):
+        return expr1 is expr2
+    if not isExpr(expr1) or not isExpr(expr2):
+        assert not isExpr(expr1) and not isExpr(expr2)
+        return expr1 == expr2
+
+    assert compare_nested_exprs(expr1.lhs, expr2.lhs)
+    assert expr1.op == expr2.op
+    if expr1.op == "BETWEEN":
+        assert compare_rhs_of_between_op(expr1.rhs, expr2.rhs)
+    else:
+        assert compare_nested_exprs(expr1.rhs, expr2.rhs)
+    return True
+
+
 @pytest.mark.parametrize(
     "args,expected",
     [
-        ("hotdog; ", {"where_clause": Food.name == "hotdog"}),
         (
-            "hotdog   ;    today",
-            {
-                "where_clause": (Food.name == "hotdog")
-                & (Food.date == datetime.now().date())
-            },
-        ),
-        ("toDAY", {"where_clause": datetime.now().date()}),
-        (
-            "18",
+            'N "name" datE 1/01 t 5h sort date,name l 1',
             {
                 "where_clause": (
-                    Food.date == datetime(day=1, month=now().month, year=now().year)
-                )
-            },
-        ),
-        (
-            "18/5",
-            {
-                "where_clause": (
-                    Food.date == datetime(day=1, month=5, year=now().year)
-                )
-            },
-        ),
-        (
-            "18/5/2000",
-            {"where_clause": (Food.date == datetime(day=1, month=5, year=2000))},
-        ),
-        (
-            "18h",
-            {
-                "where_clause": (
-                    Food.time.between(
-                        time(hour=18, minute=00), time(hour=18, minute=59)
-                    )
-                )
-            },
-        ),
-        (
-            "18:55",
-            {"where_clause": (Food.time == time(hour=18, minute=55))},
-        ),
-        ("sort date", {"sorting_clause": Food.date}),
-        ("order BY -date", {"sorting_clause": Food.date.desc()}),
-        ("LIMIT 5", {"limit_clause": "5"}),
-        (
-            "food   ; 1/01 5h    sort  date LImit 1",
-            {
-                "where_clause": (
-                    (Food.name == "food")
+                    (Food.name == "name")
                     & (Food.date == datetime(day=1, month=1, year=now().year))
+                    & Food.time.between(
+                        time(hour=5, minute=0),
+                        time(hour=5, minute=59),
+                    )
+                ),
+                "sort_clause": [Food.date.asc(), Food.name.asc()],
+                "limit_clause": 1,
+            },
+        ),
+        # the following 4 cases shows order doesn't matter for where_clause
+        (
+            "name `name` time 5:05 date 12/12",
+            {
+                "where_clause": (
+                    (Food.name == "name")
+                    & (Food.date == datetime(day=12, month=12, year=now().year))
+                    & (Food.time == time(hour=5, minute=5))
+                ),
+                "sort_clause": None,
+                "limit_clause": None,
+            },
+        ),
+        (
+            "time 5:05 name `name` date 12/12",
+            {
+                "where_clause": (
+                    (Food.name == "name")
+                    & (Food.date == datetime(day=12, month=12, year=now().year))
+                    & (Food.time == time(hour=5, minute=5))
+                ),
+                "sort_clause": None,
+                "limit_clause": None,
+            },
+        ),
+        (
+            "date 12/12 time 5:05 name `name`",
+            {
+                "where_clause": (
+                    (Food.name == "name")
+                    & (Food.date == datetime(day=12, month=12, year=now().year))
+                    & (Food.time == time(hour=5, minute=5))
+                ),
+                "sort_clause": None,
+                "limit_clause": None,
+            },
+        ),
+        (
+            "t 5h n 'name'",
+            {
+                "where_clause": (
+                    (Food.name == "name")
                     & Food.time.between(
                         time(hour=5, minute=00), time(hour=5, minute=59)
                     )
                 ),
-                "sorting_clause": Food.date,
-                "limit_clause": "1",
+                "sort_clause": None,
+                "limit_clause": None,
+            },
+        ),
+        (
+            # shows where_clause is optional
+            "sort time limit 5",
+            {
+                "where_clause": None,
+                "sort_clause": [Food.time.asc()],
+                "limit_clause": 5,
+            },
+        ),
+        (
+            "limit 5",
+            {
+                "where_clause": None,
+                "sort_clause": None,
+                "limit_clause": 5,
+            },
+        ),
+        (
+            "",
+            {
+                "where_clause": None,
+                "sort_clause": None,
+                "limit_clause": None,
             },
         ),
     ],
 )
-def test_parse_args_given_valid_args(args, expected):
+def test_parse_given_valid_args(args, expected):
     parser = FoodParser(args)
     parser.parse()
 
-    clauses = ["where_clause", "sorting_clause", "limit_clause"]
-    for key in expected:
-        assert bool(getattr(parser, key) == expected[key])
-        clauses.remove(key)
+    assert compare_nested_exprs(parser.where_clause, expected["where_clause"])
+    assert parser.sort_clause == expected["sort_clause"]
+    assert parser.limit_clause == expected["limit_clause"]
 
-    for clause in clauses:
-        assert getattr(parser, clause) is None
+
+@pytest.mark.parametrize(
+    "args,error,invalid_value",
+    # fmt: off
+    [
+        ("date",                  InvalidValue,      ""),
+        ("date not_a_date",       InvalidValue,      "not_a_date"),
+        ("date not_a_date 05/05", InvalidValue,      "not_a_date"),
+        ("date date 10/10",       InvalidValue,      "date"),
+        ("time date 18/5",        InvalidValue,      "date"),
+        ("date foo 10:10",        InvalidValue,      "foo"),
+        ("foo",                   InvalidExpression, "foo"),
+        ("foo 10:10",             InvalidExpression, "foo 10:10"),
+        ("foo date 10/10",        InvalidExpression, "foo"),
+        ("foo bar date 10/10",    InvalidExpression, "foo bar"),
+        ("time 12:59 foo",        InvalidExpression, "foo"),
+        ("date 18/5 date 18/5",   InvalidExpression, "date 18/5"),
+        ("date 18/5 date",        InvalidExpression, "date"),
+        ("date 18/5 18/5",        InvalidExpression, "18/5"),
+    ],
+    # fmt: on
+)
+def test_parse_given_invalid_args(args, error, invalid_value):
+    parser = FoodParser(args)
+    with pytest.raises(error, match=rf".+?'{invalid_value}'.+"):
+        parser.parse()
+
+
+@pytest.mark.parametrize(
+    "string,expr_name,expected",
+    [
+        (" date sort ", "sort", None),
+        (" dATe sort ", "date", "dATe"),
+        (" tIme date ", "date", None),
+        (" tiME dAte ", "time", "tiME"),
+        (" s date ", "date", None),
+        (" s date ", "sort", "s"),
+        (" n time ", "time", None),
+        (" N time ", "name", "N"),
+    ],
+)
+def test_search_keyword(string, expr_name, expected):
+    parser = FoodParser("")
+    for expr in parser.exprs:
+        if expr["name"] == expr_name:
+            pattern = expr["keyword_pattern"]
+
+    if expected is None:
+        with pytest.raises(KeywordNotFound):
+            parser.search_keyword(pattern, string)
+    else:
+        match, *_ = parser.search_keyword(pattern, string)
+        assert match == expected
+
+
+@pytest.mark.skip
+def test_ends_with_keyword():
+    pass
+
+
+@pytest.mark.parametrize(
+    "args,expected",
+    [
+        ([1], 1),
+        ([0], 0),
+        ([1, 1], 1),
+        ([1, 0], 0),
+        ([0, 1], 0),
+        ([0, 0], 0),
+    ],
+)
+def test_add_to_where_clause_given_valid_args(args, expected):
+    parser = FoodParser("")
+    for arg in args:
+        parser.add_to_where_clause(arg)
+    assert parser.where_clause == expected
+
+
+@pytest.mark.parametrize(
+    "args,expected",
+    [
+        ('"hotdog"', Food.name == "hotdog"),
+        ("`hotdog`", Food.name == "hotdog"),
+        ("'hotdog'", Food.name == "hotdog"),
+        ("'  hotdog  '", Food.name == "  hotdog  "),
+    ],
+)
+def test_parse_name_given_valid_args(args, expected):
+    parser = FoodParser("")
+    parser.parse_name(args)
+    assert compare_nested_exprs(parser.where_clause, expected)
+
+
+@pytest.mark.parametrize(
+    "args",
+    [
+        "''",
+        "hotdog",  # no quotes
+        "`hotdog",  # single quote
+        "'hotdog\"",  # beginning and ending with different quotes
+    ],
+)
+def test_parse_name_given_invalid_args(args):
+    parser = FoodParser("")
+    with pytest.raises(InvalidName):
+        parser.parse_name(args)
+
+
+@pytest.mark.parametrize(
+    "args,expected",
+    [
+        ("today", Food.date == datetime.now().date()),
+        ("toDAY", Food.date == datetime.now().date()),
+        ("1", Food.date == datetime(day=1, month=now().month, year=now().year)),
+        ("18", Food.date == datetime(day=18, month=now().month, year=now().year)),
+        ("18/5", Food.date == datetime(day=18, month=5, year=now().year)),
+        ("1/5/2000", Food.date == datetime(day=1, month=5, year=2000)),
+    ],
+)
+def test_parse_date_given_valid_args(args, expected):
+    parser = FoodParser("")
+    parser.parse_date(args)
+    assert compare_nested_exprs(parser.where_clause, expected)
+
+
+@pytest.mark.parametrize(
+    "args,expected",
+    [
+        (
+            "18h",
+            Food.time.between(
+                time(hour=18, minute=00),
+                time(hour=18, minute=59),
+            ),
+        ),
+        ("18:55", Food.time == time(hour=18, minute=55)),
+    ],
+)
+def test_parse_time_given_valid_args(args, expected):
+    parser = FoodParser("")
+    parser.parse_time(args)
+    assert compare_nested_exprs(parser.where_clause, expected)
+
+
+@pytest.mark.parametrize("args", ["", "18"])
+def test_parse_time_given_invalid_args(args):
+    parser = FoodParser("")
+    with pytest.raises(InvalidTime):
+        parser.parse_time(args)
+
+
+@pytest.mark.parametrize(
+    "args,expected",
+    [
+        (["a"], ["a"]),
+        (["a", 1], ["a", 1]),
+    ],
+)
+def test_add_to_sort_clause_given_valid_args(args, expected):
+    parser = FoodParser("")
+    for arg in args:
+        parser.add_to_sort_clause(arg)
+    assert parser.sort_clause == expected
+
+
+@pytest.mark.parametrize(
+    "args,expected",
+    [
+        ("date", [Food.date.asc()]),
+        ("-date", [Food.date.desc()]),
+        ("date   ,  -name", [Food.date.asc(), Food.name.desc()]),
+        ("date,-name", [Food.date.asc(), Food.name.desc()]),
+    ],
+)
+def test_parse_sort_given_valid_args(args, expected):
+    parser = FoodParser("")
+    parser.parse_sort(args)
+    assert parser.sort_clause == expected
+
+
+@pytest.mark.parametrize(
+    "args",
+    [
+        "",
+        "time,",  # ends with comma
+        "   time",
+        "time  ",
+        "zzzz",  # invalid Food field
+    ],
+)
+def test_parse_sort_given_invalid_args(args):
+    parser = FoodParser("")
+    with pytest.raises(InvalidSortField):
+        parser.parse_sort(args)
+
+
+@pytest.mark.parametrize("args,expected", [("5", 5), ("0", 0)])
+def test_parse_limit_given_valid_args(args, expected):
+    parser = FoodParser("")
+    parser.parse_limit(args)
+    assert parser.limit_clause == expected
+
+
+@pytest.mark.parametrize("args", ["a", "", "-4", "5.0"])
+def test_parse_limit_given_invalid_args(args):
+    parser = FoodParser("")
+    with pytest.raises(InvalidLimit):
+        parser.parse_limit(args)
